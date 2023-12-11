64cc5b7866648caaa3410a7554a6a8245311a08a
David Aryasetia <david.aryasetia@gmai.com>
2023-12-11 15:38:41 +0700

    view,update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,9 @@ use Illuminate\Http\RedirectResponse;
 // return type view
 use Illuminate\View\View;
 
+// import Facade "Storage"
+use Illuminate\Support\Facades\Storage;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -61,7 +64,58 @@ class PostController extends Controller
         $post = Post::findOrFail($id);
 
         // render view with post
-        return view('post.show', compact('post'));
+        return view('posts.show', compact('post'));
 
     }
+
+    // membuat method baru dengan nama edit untuk get data by id untuk edit
+    public function edit(string $id): View{
+        // get data by ID
+        $post = Post::findOrFail($id);
+
+        // render view with post
+        return view('posts.edit', compact('post'));
+    }
+
+    // membuat method dengan nama update untuk mengupdate data ke dalam database
+    public function update(Request $request, $id): RedirectResponse {
+
+        // membuat validasi terlebih dahulu
+        $this->validate($request, [
+            'image' => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        // get data by id
+        $post = Post::findOrFail($id);
+
+        // membuat kondisi untuk memastikan request file bernama image
+        if($request->hasFile('image')){
+            // upload gambar baru
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // hapus old image
+            Storage::delete('public/posts/'.$post->image);
+
+            // update semua data ke Database
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+
+        } else {
+            // update tanpa image jika tidak ada request image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        }
+
+        // redirect dan mengarahkan ke index
+        return redirect()->route('posts.index')->with(['success'=>'Data Berhasil Diubah']);
+    }
+
 }
